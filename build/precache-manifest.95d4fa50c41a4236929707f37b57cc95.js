0a661955d88780a9ce62d5159793fc4f924e320e
MuhammadAffanAli123 <affanali714@yahoo.com>
2020-08-06 06:51:56 -0700

    ERROR RESOLVE

---
+++ build/precache-manifest.95d4fa50c41a4236929707f37b57cc95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f2119efae3ad1b0bcf4f709cc9236c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10bad02465aba498ed06",
+    "url": "/static/css/main.bd4f3cda.chunk.css"
+  },
+  {
+    "revision": "c9a61d1601b02c76051c",
+    "url": "/static/js/2.deeaff8a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.deeaff8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10bad02465aba498ed06",
+    "url": "/static/js/main.4f10335c.chunk.js"
+  },
+  {
+    "revision": "af178cef4ccb6cce0331",
+    "url": "/static/js/runtime-main.e4a83928.js"
+  }
+]);
